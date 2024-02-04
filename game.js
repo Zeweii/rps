6055d142b057a5c9decc6862f9bf0b5b9ee56515
Zachary Jiang <zacharyjiang720@gmail.com>
2024-02-04 23:59:45 +1100

    get computer choice

---
+++ game.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    var number = Math.floor(Math.random() * 3)
+
+    switch (number) {
+        case 0:
+            return "Rock"
+            
+    }
+    return
+}
